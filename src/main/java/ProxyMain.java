c8768996a6b3abe0d66673d1d29a34fa07d160c4
paik <melon9751@naver.com>
2021-09-14 18:04:46 +0900

    프록시 객체로 조회하는 테스트 추가

---
+++ src/main/java/ProxyMain.java
@@ -0,0 +1,37 @@
+import domain.Member;
+import domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class ProxyMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setName("memberA");
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member savedMember = em.getReference(Member.class, member.getId());
+            System.out.println(savedMember.getClass());
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
