63df98d8da2b76d9c5ed761b9bfc2231686706cb
paik <melon9751@naver.com>
2021-09-18 16:06:07 +0900

    컬렉션 값 타입 지연로딩 전략 테스트 추가

--- src/main/java/CollectionMain.java
+++ src/main/java/CollectionMain.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class CollectionMain {
     public static void main(String[] args) {
@@ -18,11 +20,26 @@ public class CollectionMain {
             member.setName("member1");
             member.setAge(20);
 
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
             member.setHomeAddress(new Address("city", "zip", "street"));
             member.getAddressHistory().add(new Address("old1", "zip", "street"));
             member.getAddressHistory().add(new Address("old1", "zip", "street"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            System.out.println(addressHistory);
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            System.out.println(favoriteFoods);
 
             tx.commit();
         } catch(Exception e) {
