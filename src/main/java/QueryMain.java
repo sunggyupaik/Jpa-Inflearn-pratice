4a74841de0c9d92ab522466ddbfb4458ef079387
paik <melon9751@naver.com>
2021-09-20 09:29:43 +0900

    JPQL 조회 결과와 영속성 컨텍스트 관계 테스트 추가
    JPQL로 조회한 객체는 영속성 컨텍스트에 있어서 나이 수정을 통해 확인하는
    테스트를 만듭니다

---
+++ src/main/java/QueryMain.java
@@ -0,0 +1,40 @@
+import domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class QueryMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            Member member1 = new Member();
+            member1.setName("memberA");
+            member1.setAge(15);
+            em.persist(member1);
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
