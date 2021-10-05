e447cd09aa66fad5d1ea2160d0b10946cb510ecf
melon9751@naver.com <melon9751@naver.com>
2021-10-06 01:03:06 +0900

    lazy 로딩으로 페이징 처리하기
    lazy 로딩으로 페이징 처리 테스트를 추가합니다

---
+++ src/main/java/FetchJoin2.java
@@ -0,0 +1,69 @@
+import domain.Member;
+import domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class FetchJoin2 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member memberA = new Member();
+            memberA.setName("회원1");
+            memberA.setTeam(teamA);
+            em.persist(memberA);
+
+            Member memberB = new Member();
+            memberB.setName("회원2");
+            memberB.setTeam(teamA);
+            em.persist(memberB);
+
+            Member memberC = new Member();
+            memberC.setName("회원3");
+            memberC.setTeam(teamB);
+            em.persist(memberC);
+
+            em.flush();
+            em.clear();
+            
+            //단순하게 lazy 로딩으로 페이징 처리하기
+           String query = "select t From Team t";
+
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
+
+            for(Team team : result) {
+                System.out.println("teamName=" + team.getName() + ","
+                        + "members=" + team.getMembers().size());
+                for(Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+            }
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
