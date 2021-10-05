e3f2e7b04137277cc4ca457ed3139ddb11211880
melon9751@naver.com <melon9751@naver.com>
2021-10-05 20:12:34 +0900

    연관필드 조회 테스트를 추가
    Team과 Member 연관관계 사이에서 필드 조회를 추가합니다

---
+++ src/main/java/getMain.java
@@ -0,0 +1,49 @@
+import domain.Child;
+import domain.Member;
+import domain.Parent;
+import domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class getMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            Team team = new Team();
+            team.setName("팀A");
+            em.persist(team);
+
+            Member memberA = new Member();
+            memberA.setTeam(team);
+            team.getMembers().add(memberA);
+            em.persist(memberA);
+
+            em.flush();
+            em.clear();
+
+            String query = "SELECT m.team.name FROM Member m";
+            List<String> result = em.createQuery(query, String.class)
+                    .getResultList();
+
+            for(String s : result) {
+                System.out.println(s);
+            }
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
